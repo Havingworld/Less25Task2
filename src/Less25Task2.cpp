373a5611aed56a91a08467521fc5cd3916c2f08f
bigsi <bigsi@HVWLD_P415>
2024-10-21 14:51:53 +1000

    Добавьте файлы проекта.

---
+++ src/Less25Task2.cpp
@@ -0,0 +1,12 @@
+﻿// Less25Task2.cpp: определяет точку входа для приложения.
+//
+
+#include "Less25Task2.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
