d511616e83a13d1161759bb2fa71fe516d9845d8
havingworld <havingworld@gmail.com>
2024-10-24 16:03:45 +1000

    123

--- src/Less25Task2.cpp
+++ src/Less25Task2.cpp
@@ -1,12 +1,33 @@
-﻿// Less25Task2.cpp: определяет точку входа для приложения.
-//
-
-#include "Less25Task2.h"
+﻿#include "cpu.h"
+#include "disk.h"
+#include "gpu.h"
+#include "kbd.h"
+#include "ram.h"
 
 using namespace std;
 
-int main()
-{
-	cout << "Hello CMake." << endl;
+int main() {
+    std::string command{""};
+    while (command != "exit") {
+        std::cout << "Enter command (sum, save, load, input, display, exit): ";
+        std::cin >> command;
+
+        if (command == "sum") {
+            CPUcompute();
+        }
+        else if (command == "save") {
+            diskSave(*buffer);
+        }
+        else if (command == "load") {
+            diskLoad();
+        }
+        else if (command == "input") {
+            keyboardInput();
+        }
+        else if (command == "display") {
+            gpuDisplay();
+        }
+    }
+
 	return 0;
 }
